f61093104eb3e7267d53c8ab5bcb09a8e2818dfd
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-08 16:59:47 +0100

    change year next prev with usestate onclick event

--- src/components/ExerciseComponent.js
+++ src/components/ExerciseComponent.js
@@ -1,12 +1,32 @@
-import React from 'react'
 
-export const ExerciseComponent = ({year}) => {
+import React, {useState} from 'react'
+
+export const ExerciseComponent = ({ year }) => {
+  
+  const [yearNow, setYearNow] = useState(year);
+
+  const next = e => {
+    setYearNow(yearNow + 1);
+  }
+
+  const backward = e => {
+    setYearNow(yearNow - 1);
+  };
+
+
   return (
     <div>
       <h2>Exercise with Events and UseState</h2>
-      <strong className='label label-green'>
-          {year}
-      </strong>
+      <strong className="label label-green">{yearNow}</strong>
+      <p>
+        <button onClick={backward}>
+          <i className="fa-solid fa-backward"></i>
+        </button>
+        &nbsp;
+        <button onClick={next}>
+          <i className="fa-solid fa-forward"></i>
+        </button>
+      </p>
     </div>
-  )
+  );
 }
